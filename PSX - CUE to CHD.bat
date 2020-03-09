b3aa63270db977d870583f75b8e5c9235aadb47e
grioghar <thegrio@gmail.com>
2020-03-08 22:41:05 -0500

    PSX handling much improved
    Filename changes are coming, as the PSX file now handles *.7z, *.zip, *.rar, *.iso, and *.cue. Run this from  the root folder with the folder name. eg "PSX - CUE to CHD.bat" "<path to PSX roms>

--- PSX - CUE to CHD.bat
+++ PSX - CUE to CHD.bat
@@ -2,22 +2,31 @@
 REM for /r %%i in (*.cue, *.gdi) do chdman.exe createcd -i "%%i" -o "%%~ni.chd" --force
 REM timeout /t 10
 
-set "PATH=%PATH%;C:\Program Files\7-Zip\"
-set rpie=R:\psx
+call config.bat
 set rompath=%1
 
-::set rompath=%rompath:"=%
+:: Handle compressed files
 for /r %1 %%z in (*.7z, *.zip, *.rar) DO ( 
-	if exist "%rpie%\%%~nz.chd" (
-		rem file exists
+	if exist "%rpi%\%%~nz.chd" (
+		echo  %%~nz.chd exists on %rpi%
 		) else (
 		7z x -y "%%z" -o%1
 		chdman.exe createcd -i "%rompath:"=%\%%~nz.cue" -o "%rompath:"=%\%%~nz.chd" --force
-		echo Copying %%~nz.chd to %rpie%  
-		move /y "%rompath:"=%\%%~nz.chd" %rpie%
+		echo Copying %%~nz.chd to %rpi%  
+		move /y "%rompath:"=%\%%~nz.chd" %rpi%
 			
 		del "%rompath:"=%\%%~nz.cue"
 		del "%rompath:"=%\%%~nz.bin"
 		)
 	)
 
+:: Handle uncompressed files
+for /r %1 %%z in (*.cue, *.iso) DO (
+	if exist "%rpi%\%%~nz.chd" (
+		 %%~nz.chd exists on %rpi%
+		) else (
+		chdman.exe createcd -i "%rompath:"=%\%%~nz.cue" -o "%rompath:"=%\%%~nz.chd" --force
+		echo Copying %%~nz.chd to %rpi%  
+		move /y "%rompath:"=%\%%~nz.chd" %rpi%
+		)
+	)
--- config.bat
+++ config.bat
@@ -2,8 +2,11 @@
 
 :: how to use - call config.bat in header
 
-set %rpiip%="IP Address of Raspberry Pie"
-set %rpiusername%="Username (default is 'pi')"
-set %rpipassword%="Password (default is 'raspberry')"
+set rpi=R:\psx
+set rompath=%1
+
+set rpiip="IP Address of Raspberry Pie"
+set rpiusername="Username (default is 'pi')"
+set rpipassword="Password (default is 'raspberry')"
 
 set "PATH=%PATH%;C:\Program Files\7-Zip\"
