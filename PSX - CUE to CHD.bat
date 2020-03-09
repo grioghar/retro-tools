3a182dfddf3393218848ce668f89badd3d7a21e8
grioghar <thegrio@gmail.com>
2020-03-08 19:05:01 -0500

    added logging capability
    logging, recursion - PCECD-APE-to-WAV complete. Moving on to PSX-7z-to-CUE-to-CHD script.

--- PSX - CUE to CHD.bat
+++ PSX - CUE to CHD.bat
@@ -1,12 +1,14 @@
 REM for /r %%i in (*.cue, *.gdi) do chdman.exe createcd -i "%%i" -o "%%~ni.chd" --force
 REMtimeout /t 10
 
-FOR /r %%i IN (*.cue, *.gdi) DO (
-
-if exist "R:\psx\%%~ni.chd" (
-    rem file exists
-) else (
- chdman.exe createcd -i "%%i" -o "%%~ni.chd" --force
-  move /y "%%~ni.chd" R:\psx
-)
+for /r %1 %%z IN (*.7z, *.zip) DO ( 
+	7z x "%%z" -o%1
+	FOR /r %%i IN (*.cue, *.gdi) DO (
+		if exist "R:\psx\%%~ni.chd" (
+		rem file exists
+		) else (
+		chdman.exe createcd -i "%%i" -o "%%~ni.chd" --force
+	    move /y "%%~ni.chd" R:\psx
+		)
+	)	
 )
